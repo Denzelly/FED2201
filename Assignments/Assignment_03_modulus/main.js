c7c0328deddd8ccd8cdc1091195b365c706e2252
Denzel Brown <denzelbrown@Denzels-MacBook-Air.local>
2022-01-19 05:17:12 -0500

    message attempt

--- Assignments/Assignment_03_modulus/main.js
+++ Assignments/Assignment_03_modulus/main.js
@@ -3,17 +3,28 @@ console.log('n1 = ' + n1);
 var n2 = prompt('And what number would you like it divided by?')
 console.log('n2 = ' + n2);
 // var n = (n1-n2);
-var n=n1-n2;
+var n = n1 - n2;
 
 (function () {
-    for (let i = 0;;i++) {
-        if (n > 0) {
-            n=n1
-            n1-=n2
+        for (let i = 0;; i++) {
+            if (n > 0) {
+                n = n1
+                n1 -= n2
+            } else {
+                break
+            }
+            console.log('Result: ' + i + '; Remainder: ' + n)
         }
-        else {break}
-        console.log('Result: ' + i + '; Remainder: ' + n)
-    } 
+    //     for (let i = 0;; i++) {
+    //     if (n1 = 0) {
+    //         console.log('Error: undefined')
+    //     }
+    //         // else {break}
+    //     if (n2 = 0) {
+    //         console.log('Error: Cannot Divide By Zero!')
+    //     }
+    //         else {break}
+    // }
 })();
 
 // let n1=15
