b87bd9f78a2085c0f44c295ea110306192aea3e3
Denzel Brown <denzelbrown@Denzels-MacBook-Air.local>
2022-02-25 00:30:28 -0500

    toh commit

--- Assignments/Assignment_11_tour_of_heroes_tut/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Assignments/Assignment_11_tour_of_heroes_tut/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
-import { Hero } from './../hero';
+import { HEROES } from './../mock.heroes';
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {id: 1, name: 'Windstorm' };
+heroes = HEROES;
+selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    console.log(this.selectedHero)
   }
 
 }
