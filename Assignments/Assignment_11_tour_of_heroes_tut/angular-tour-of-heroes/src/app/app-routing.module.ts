7f2e3ff7637cc500e35f6f2998af1b267ffa6ac3
Denzel Brown <denzelbrown@Denzels-MacBook-Air.local>
2022-02-25 02:26:58 -0500

    almost finished

--- Assignments/Assignment_11_tour_of_heroes_tut/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ Assignments/Assignment_11_tour_of_heroes_tut/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { HeroDetailComponent } from './../../../../../Code_alongs/week08_angular/day02/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
