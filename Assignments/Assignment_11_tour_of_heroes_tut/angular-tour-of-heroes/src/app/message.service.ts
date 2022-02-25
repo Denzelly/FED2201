faa8274ee6b16e307ab5cf4eafac43d02f14437a
Denzel Brown <denzelbrown@Denzels-MacBook-Air.local>
2022-02-25 02:06:23 -0500

    toh debugged

--- Assignments/Assignment_11_tour_of_heroes_tut/angular-tour-of-heroes/src/app/message.service.ts
+++ Assignments/Assignment_11_tour_of_heroes_tut/angular-tour-of-heroes/src/app/message.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
-  messages?: string[] = []
+  messages: string[] = []
 
   constructor() { }
 
@@ -16,3 +16,4 @@ export class MessageService {
     this.messages = []; 
   }
 }
+
