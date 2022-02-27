d2941724ab2d04f94c6ce0eb20a99740115408a6
Denzel Brown <denzelbrown@Denzels-MacBook-Air.local>
2022-02-26 22:49:42 -0500

    working on date calculator

--- Assignments/Assignment_10_coding_practice3/main.js
+++ Assignments/Assignment_10_coding_practice3/main.js
@@ -1,30 +1,51 @@
-var thatDate = new Date(2030,0)
+var thatDate = new Date(2030, 0)
 var today = new Date()
+
 var fromNow = document.getElementById('fromNow')
-var tilDate = document.getElementById('tilDate');
+
 var daysLeft = document.getElementById('daysLeft');
 var checkButton1 = document.getElementById('check1')
 var checkButton2 = document.getElementById('check2')
 
-
-var startDate = document.getElementById('startDate');
-var endDate = document.getElementById('endDate');
 var daysBt = document.getElementById('daysBt');
 var checkButton3 = document.getElementById('check3')
 
 
-checkButton1.onclick = function() {check1()};
+checkButton1.onclick = function () {
+    check1()
+};
+
 function check1() {
-    fromNow.value = parseInt((thatDate-today)/1000/60/60/24);
+    fromNow.value = parseInt((thatDate - today) / 1000 / 60 / 60 / 24);
 }
 
-checkButton2.onclick = function() {check2()};
-function check2() {
-    console.log(tilDate)
-    daysLeft.value = parseInt((thatDate-tilDate)/1000/60/60/24);
-}
 
-checkButton3.onclick = function() {check3()};
+
+document.getElementById('tilDate').addEventListener('change', function () {
+    let newTilDate = new Date(this.value);
+
+    checkButton2.onclick = function () {
+        check2()
+    };
+
+    function check2() {
+        daysLeft.value = parseInt((thatDate - newTilDate-100800000) / 1000 / 60 / 60 / 24);
+    }
+});
+
+
+
+checkButton3.onclick = function () {getDates()};
+
+function getDates() {
+    document.getElementById('startDate').addEventListener('change', function () {
+        let newStartDate = new Date(this.value)
+    });
+    document.getElementById('endDate').addEventListener('change', function () {
+        let newEndDate = new Date(this.value)
+    });
+
 function check3() {
-    daysBt.value = Math.abs(parseInt((startDate-endDate)/1000/60/60/24));
+    daysBt.value = Math.abs(parseInt((newStartDate - newEndDate) / 1000 / 60 / 60 / 24));
+}
 }
