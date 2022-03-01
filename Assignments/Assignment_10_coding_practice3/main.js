7f597bd483e476cd153049425cc33f5b0f02c18f
Denzel Brown <denzelbrown@Denzels-MacBook-Air.local>
2022-03-01 15:56:34 -0500

    catchup

--- Assignments/Assignment_10_coding_practice3/main.js
+++ Assignments/Assignment_10_coding_practice3/main.js
@@ -1,3 +1,4 @@
+// Variable Declarations
 var thatDate = new Date(2030, 0)
 var today = new Date()
 
@@ -10,44 +11,37 @@ var checkButton2 = document.getElementById('check2')
 var daysBt = document.getElementById('daysBt');
 var checkButton3 = document.getElementById('check3')
 
-
-checkButton1.onclick = function () {
-    check1()
-};
+// Part 1 - From Now
+checkButton1.onclick = function () {check1()};
 
 function check1() {
     fromNow.value = parseInt((thatDate - today) / 1000 / 60 / 60 / 24);
 }
 
 
-
+// Part 2 - From Input
 document.getElementById('tilDate').addEventListener('change', function () {
     let newTilDate = new Date(this.value);
 
-    checkButton2.onclick = function () {
-        check2()
-    };
+    checkButton2.onclick = function () {check2()};
 
     function check2() {
-        console.log(thatDate-today)
-        console.log(thatDate-newTilDate)
-        daysLeft.value = parseInt(((thatDate-newTilDate-100000000)/1000/60/60/24));
+        console.log(thatDate - today)
+        console.log(thatDate - newTilDate)
+        daysLeft.value = parseInt(((thatDate - newTilDate - 100000000)/1000/60/60/24));
     }
 });
 
 
+// Part 3 - Days Between
+document.getElementById('startDate').addEventListener('change', function () {
+    let newStartDate = new Date(this.value)
+    document.getElementById('endDate').addEventListener('change', function () {
+            var newEndDate = new Date(this.value)
+        });
 
-// checkButton3.onclick = function () {getDates()};
-
-// function getDates() {
-//     document.getElementById('startDate').addEventListener('change', function () {
-//         let newStartDate = new Date(this.value)
-//     });
-//     document.getElementById('endDate').addEventListener('change', function () {
-//         let newEndDate = new Date(this.value)
-//     });
-
-// function check3() {
-//     daysBt.value = Math.abs(parseInt((newStartDate - newEndDate) / 1000 / 60 / 60 / 24));
-// }
-// }
+    checkButton3.onclick = function () {check3()};
+    function check3() {
+        daysBt.value = Math.abs(parseInt((newStartDate - newEndDate)/1000/60/60/24));
+    }
+});
